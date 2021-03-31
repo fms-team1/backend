ecf1d94df83ad0a5a2b107d279cc9959e53e6253
damirbek1997 <damir.1997@mail.ru>
2021-03-31 17:28:46 +0600

    removing endpoint basic-error-controller

--- src/main/java/kg/neobis/fms/configs/SwaggerConfig.java
+++ src/main/java/kg/neobis/fms/configs/SwaggerConfig.java
@@ -2,6 +2,7 @@ package kg.neobis.fms.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiKey;
@@ -24,7 +25,7 @@ public class SwaggerConfig {
                 .securityContexts(Collections.singletonList(securityContext()))
                 .securitySchemes(Collections.singletonList(apiKey()))
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
                 .build();
     }
